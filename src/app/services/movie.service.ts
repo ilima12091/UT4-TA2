463b1bc4813d1e336f7254bd56f01e21ff36d9cc
RobottiFranco <franco.robotti0@gmail.com>
2023-05-13 19:49:04 -0300

    movie service ahora trae las peliculas al label

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,27 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Card } from '../interfaces/card';
-import { CardComponent } from '../components/card/card.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private apiKey = '8957f5f8f34a49441a27c556adb5a068';
+  private MOVIES_IMAGES_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
-    MOVIES_BASE_URL = "https://api.themoviedb.org/3";
-    MOVIES_IMAGES_BASE_URL = "https://image.tmdb.org/t/p/w500"
-    API_KEY = "8957f5f8f34a49441a27c556adb5a068";
+  constructor() { }
 
-    async loadMovies() {
-        try {
-            const response = await (await fetch(`${this.MOVIES_BASE_URL}/movie/top_rated?api_key=${this.API_KEY}`)).json();
-            return response;        
-        } catch(error) {
-            alert("Ocurrió un error al cargar las películas")
-            return null;
-        }
-    }
-
-    submitApplication() {
-
-    }
+  async getTopRatedMovies() {
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${this.apiKey}`;
+    const response = await fetch(url);
+      return await response.json();
+  }
 }
